a5f38f5db7fb05a0c50bdde92bf5cc2d638a6e13
Louis Riel <louis-sebastien.riel@ca.ibm.com>
2020-04-30 22:28:25 -0400

    Fixed typo

--- standitup.sh
+++ standitup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 has_docker_access="$(docker-compose ps &> /dev/null && echo yes || echo no)"
 [[ "$has_docker_access" == "no" ]] && echo Your account does not have sufficient rights to run this script, please run the script using sudo && exit 4
-chown -R s963:20000 grafana/serverconfig
+chown -R 963:20000 grafana/serverconfig
 chown -R 964:1001 influxdb
 docker-compose up -d;
 docker-compose logs -f;
