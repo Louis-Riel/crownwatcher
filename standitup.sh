3eb2b3dd0742739d1ff8a42d0175933f7f304adf
Louis Riel <louis-sebastien.riel@ca.ibm.com>
2020-04-30 22:11:50 -0400

    Added empty grafana server config

--- standitup.sh
+++ standitup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+has_docker_access="$(docker-compose ps &> /dev/null && echo yes || echo no)"
+[[ "$has_docker_access" == "no" ]] && echo Your account does not have sufficient rights to run this script, please run the script using sudo && exit 4
 chown -R 963:20000 grafana
 chown -R 964:1001 grafana
+docker-compose up -d;
+docker-compose logs -f;
